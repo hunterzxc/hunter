3a8abb3b74d293dd0df57b32af4e61f4dd1b74d2
hunterzxc <60175173+hunterzxc@users.noreply.github.com>
2020-02-11 18:44:52 +0800

    Add files via upload

---
+++ rss.php
@@ -0,0 +1,19 @@
+<?php
+$domOBJ = new DOMDocument();
+$domOBJ->load("https://app-htr.herokuapp.com/");
+
+$content = $domOBJ->getElementsByTagName("item");
+
+foreach ($content as $data) {
+
+    $username = $data->getElementsByTagName("id")->item(0)->nodeValue;
+    $email = $data->getElementsByTagName("username")->item(0)->nodeValue;
+    $address = $data->getElementsByTagName("password")->item(0)->nodeValue;
+
+    echo "
+    <ul>
+    <li>Id: <strong>$id</strong></li>
+    <li>Username: <strong>$username</strong></li>
+    <li>Password: <strong>$password</strong></li>
+    </ul>";
+}
